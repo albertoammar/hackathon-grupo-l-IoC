79f4351d2fd138ddf281ecfd41730c9683ae0b14
Alberto Ammar <albertoammarw@gmail.com>
2023-04-01 12:07:43 -0300

    feat: add repository ecr

---
+++ ecr.tf
@@ -0,0 +1,36 @@
+resource "aws_ecr_repository" "back" {
+  name                 = "grupo-l-api"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "api" {
+  repository = aws_ecr_repository.back.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep last 5 images",
+            "selection": {
+                "tagStatus": "tagged",
+                "tagPrefixList": ["v"],
+                "countType": "imageCountMoreThan",
+                "countNumber": 5
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+output "repository" {
+  value = aws_ecr_repository.back.repository_url
+}
